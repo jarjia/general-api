aafa7d422e451c6fd8ba8e0304dd12bd1f198aff
jarji <jarjaabua@gmail.com>
2023-11-25 16:24:17 +0400

    feat: handle cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,9 @@ class Cors
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT');
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin')
+            ->header('Access-Control-Allow-Methods', 'POST, PUT');;
     }
 }
